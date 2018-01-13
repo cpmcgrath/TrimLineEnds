11ef99b79474a4a1df894e05d5d0ea8f17cf3b73
Chris McGrath <admin@cpmcgrath.com>
2018-01-14 07:04:47 +1000

    #6

---
+++ buildhelpers.ps1
@@ -0,0 +1,25 @@
+function Replace($file, $before, $after)
+{
+    $content = Get-Content $file | Foreach-Object {$_ -replace $before, $after }
+    $content | Set-Content $file -Encoding UTF8
+}
+
+function UpdateVersion($project, $version)
+{
+    $file = "./$project/Properties/AssemblyInfo.cs"
+    Replace $file "AssemblyVersion\s*\([^\)]+\)"     "AssemblyVersion    (`"$version`")"
+    Replace $file "AssemblyFileVersion\s*\([^\)]+\)" "AssemblyFileVersion(`"$version`")"
+}
+
+function UpdateVsVersion($project, $version)
+{
+    UpdateVersion $project $version
+        
+    Replace "./$project/Package.cs" `
+            "\[InstalledProductRegistration\(`"#110`", `"#112`", `"[^`"]+`", IconResourceID = 400\)\]" `
+            "[InstalledProductRegistration(`"#110`", `"#112`", `"$version`", IconResourceID = 400)]"
+        
+    Replace "./$project/source.extension.vsixmanifest" `
+            "Id=`"TrimLineEnds.CMcG.370b7457-7f7a-42ab-8795-8e2faa714cc6`" Version=`"[^`"]+`"" `
+            "Id=`"TrimLineEnds.CMcG.370b7457-7f7a-42ab-8795-8e2faa714cc6`" Version=`"$version`""
+}
